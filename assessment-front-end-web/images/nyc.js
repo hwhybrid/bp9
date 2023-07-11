2540571cb06505c4fe52eb817f9762eb78ab12e0
hwhybrid <hwhybrid@gmail.com>
2023-07-11 14:58:00 -0400

    refactor: nyc.js to try new event handler approach.

--- assessment-front-end-web/images/nyc.js
+++ assessment-front-end-web/images/nyc.js
@@ -16,12 +16,36 @@ let restaurantArray = [
     "Wayla",
 ];
 
-let playButton = document.querySelector("#suggestion");
+let rouletteButton = document.querySelector("#rouletteButton");
+let exploreButton = document.querySelector(".activity");
 let restaurantRoulette = Math.floor(Math.random() * restaurantArray.length);
 
 let rouletteHandler = () => {
-    alert(`Your restaurant recommendation is: ${restaurantRoulette}`);
+    alert(
+        `Your restaurant recommendation is: ${restaurantArray[restaurantRoulette]}`
+    );
 };
 
+let activityHandler = () => {
+    let activityTips = document.createElement("p");
+    if (exploreButton.id === "music") {
+        activityTips.innerHTML = "Learn about cool NYC music here: ";
+    } else if (exploreButton.id === "food-drink") {
+        activityTips.innerHTML =
+            "Learn about cool NYC food and beverage spots here: ";
+    } else if (exploreButton.id === "art-history") {
+        activityTips.innerHTML =
+            "Learn about cool NYC art and history spots here: ";
+    } else if (exploreButton.id === "play") {
+        activityTips.innerHTML =
+            "Learn about cool NYC play spots for kids here: ";
+    } else if (exploreButton.id === "literature") {
+        activityTips.innerHTML = "Learn about cool NYC literary haunts here: ";
+    } else {
+        alert("Please make an activity selection to explore NYC.");
+    }
+    document.body.appendChild(activityTips);
+};
 
-playButton.addEventListener
+rouletteButton.addEventListener("click", rouletteHandler);
+exploreButton.addEventListener("click", activityHandler);
